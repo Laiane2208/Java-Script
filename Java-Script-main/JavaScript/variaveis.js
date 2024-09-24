14897dcc6ac44d7c111599fc20715e3dcb4d3832
Laiane <160877317+Laiane2208@users.noreply.github.com>
2024-09-24 11:59:58 -0300

    Add files via upload

---
+++ Java-Script-main/JavaScript/variaveis.js
@@ -0,0 +1,19 @@
+//Tipo Number
+
+//Var era utilizadp para todas as variáveis
+
+var numero01 = 3
+//Let é uma atualização do var e é possível a variação
+let numero02 = 10
+//Const é uma maneira de declarar uma variável que irá permanecer constante
+const numero03 = 7
+
+const soma = numero01 + numero02
+const subtracao = numero02 - numero01
+const multiplicacao = numero03 - numero02
+const divisao = numero02 / numero01
+
+console.log(soma)
+console.log(subtracao)
+console.log(subtracao)
+console.log(subtracao)
